cc1b5eca56960f398d9b3f2da84ad69f7eef8bca
Gregory Litman-Navarro <gregory.navarro@ubtrobot.com>
2023-02-08 15:33:46 -0800

    moves test code

---
+++ python/test_clean_noise.py
@@ -0,0 +1,14 @@
+from clean_noise import clean_noise
+
+# test the methods used to by top level method clean_noise
+x = get_layer(0,0,0)
+y = get_layer(0,0,1)
+z = get_layer(0,0,3)
+
+print(x)
+print(y)
+print(z)
+
+test_2d_array = [[1,2,3], [4,5,6,],[7,8,9]]
+
+# print(get_subset(test_2d_array, 0, 0, 2))
