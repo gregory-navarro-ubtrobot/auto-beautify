fde2acd86974d60e7deb0797b1e7943dd9179b39
Gregory Litman-Navarro <gregory.navarro@ubtrobot.com>
2023-02-08 16:02:38 -0800

    Lays out testing regime plan

--- python/test_clean_noise.py
+++ python/test_clean_noise.py
@@ -15,4 +15,16 @@ test_2d_array = [[1,2,3], [4,5,6,],[7,8,9]]
 
 # print(get_subset(test_2d_array, 0, 0, 2))
 
+
+# TESTING 
+# target_type will be WALL
+# replacement_type will be GROUND
+# threshold should be tested with values from 1 to 10
+# percent_positivity should be tested from 0 to 1 in increments of 0.2
+# all tests should record how different the results are from the unprocessed color matrix
+# those should be recorded to test_results/test_results.txt
+# additionally an image should be saved for each test under test_results/images/ with a number that corresponds to entry in test_results.txt
+
+
+
 print("ending test_clean_noise.py")
