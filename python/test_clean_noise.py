5f76ef960d2d82157478de30fb3fade4a3886e27
Gregory Litman-Navarro <gregory.navarro@ubtrobot.com>
2023-02-08 16:17:04 -0800

    update for import fixes

--- python/test_clean_noise.py
+++ python/test_clean_noise.py
@@ -1,17 +1,17 @@
 print("starting test_clean_noise.py")
 
-from clean_noise import clean_noise
+from clean_noise import *
 
 # test the methods used to by top level method clean_noise
-x = get_layer(0,0,0)
-y = get_layer(0,0,1)
-z = get_layer(0,0,3)
+# x = get_layer(0,0,0)
+# y = get_layer(0,0,1)
+# z = get_layer(0,0,3)
 
-print(x)
-print(y)
-print(z)
+# print(x)
+# print(y)
+# print(z)
 
-test_2d_array = [[1,2,3], [4,5,6,],[7,8,9]]
+# test_2d_array = [[1,2,3], [4,5,6,],[7,8,9]]
 
 # print(get_subset(test_2d_array, 0, 0, 2))
 
